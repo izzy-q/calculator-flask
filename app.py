378c312dd58aa70d50369b2c94228df2f5a60607
Izzy <izzyy.wq@gmail.com>
2020-09-26 10:41:46 -0400

    code cleanup
    cleaned up code according to pep8

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template, request
 import calculator
 
+
 app = Flask(__name__)
 
 
-# home page: renders index.html
+# home page
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template("index.html")
 
 
-# gets input from the home page and pass the input through the calc function in the calculator module
+# results page
 @app.route("/result/", methods=['GET', 'POST'])
 def calc():
     func = request.form['func']
